76b36af1a476838781d77a20c47aa660e4a6d1da
1010l10 <lorozco5@mail.csuchico.edu>
2022-12-04 23:19:28 -0800

    quiz 3 submission

--- tappity.cpp
+++ tappity.cpp
@@ -1,40 +1,96 @@
 #include "tappity.h"
 /**
  * Class for tappity
-**/
+ **/
 
-//Constructor sets the reference phrase
+// Constructor sets the reference phrase
 tappity::tappity(std::string reference)
 {
+  this->reference = reference;
+  something.empty = " ";
 }
 
-//Provide the input to be compared to the reference. Before this 
-//function is called, the input should be considered an empty string
+// Provide the input to be compared to the reference. Before this
+// function is called, the input should be considered an empty string
 void tappity::entry(std::string input)
 {
+
+  this->input = input;
 }
 
-//Compares the length of the reference to that of the input and
-//returns the absolute value of the difference, or 0 if they are the
-//same length
+// Compares the length of the reference to that of the input and
+// returns the absolute value of the difference, or 0 if they are the
+// same length
 int tappity::length_difference()
 {
-  return 0;
+  int _different = 0;
+  int unassigned = i;
+
+  for (i = 0; i < reference.size() && i < input.size(); i++)
+  {
+    if reference[i] != input [i])
+      _different++;
+  }
+  for (i < reference.size(); i++)
+  {
+    if (isalpha(reference[i]))
+      _different++;
+  }
+  for (i < input.size(); i++)
+  {
+    if (isalpha(input[i]))
+      _different++;
+  }
+  return _different;
 }
 
-//Compares the content of the reference to that of the input and
-//calculates the accuracy of matching characters. If the two strings
-//are identical, accuracy should be 100. However, for each 
-//corresponding character that does not match in the same location,
-//the percentage of corresponding characters should be returned.
-//For example, if the reference and input have 10 letters and the input
-//matches 8 of the corresponding reference characters, the function 
-//should return 80. When input does not have the same number of
-//characters as the reference, the accuracy should represent the percent
-//of matching characters between the shorter and longer strings. For
-//example, if one string has 8 characters matching the corresponding
-//locations in another string that has 16 characters, the accuracy is 50.
+// Compares the content of the reference to that of the input and
+// calculates the accuracy of matching characters. If the two strings
+// are identical, accuracy should be 100. However, for each
+// corresponding character that does not match in the same location,
+// the percentage of corresponding characters should be returned.
+// For example, if the reference and input have 10 letters and the input
+// matches 8 of the corresponding reference characters, the function
+// should return 80. When input does not have the same number of
+// characters as the reference, the accuracy should represent the percent
+// of matching characters between the shorter and longer strings. For
+// example, if one string has 8 characters matching the corresponding
+// locations in another string that has 16 characters, the accuracy is 50.
 double tappity::accuracy()
 {
-  return 0;
+  double score = 0;
+  double matches = 0;
+  int a = this->reference.length();
+  b = this.input.length(), i, j;
+
+  if (i == j)
+  {
+    for (i = 0; i < a; i++)
+    {
+      if (this->reference[i] == this->input[i])
+      {
+        matches++;
+      }
+    }
+    return double((matches / a) * 100);
+  }
+  else if (a < b)
+  {
+    for (i = 0; i < a; i++)
+    {
+      if (this.refrence[i] == this.input[i])
+      {
+        matches++;
+      }
+    }
+    return double((matches / a) * 100);
+  }
+  for (i = 0, i < b; i++)
+  {
+    if (this.reference[i] == this.input[i])
+    {
+      matches++;
+    }
+  }
+  return double((matches / b) * 100);
 }
--- tappityTest.cpp
+++ tappityTest.cpp
@@ -1,15 +1,78 @@
 /**
  * Unit Test suite
-**/
+ **/
 
 #include <gtest/gtest.h>
 #include "tappity.h"
- 
+
 class tappityTest : public ::testing::Test
 {
-	protected:
-		tappityTest(){} 
-		virtual ~tappityTest(){} 
-		virtual void SetUp(){}
-		virtual void TearDown(){}
+protected:
+	tappityTest() {}
+	virtual ~tappityTest() {}
+	virtual void SetUp() {}
+	virtual void TearDown() {}
 };
+
+TEST(tappityTest, tappity_plus)
+{
+	ASSERT_TRUE(true);
+	// testing for spaces in the phrase
+	//  there is a space at the start of the phrase
+	TEST(tappityTest, tappity_plus)
+	{
+		sentence R("i enjoy computer science");
+		string input = " i enjoy computer science";
+	}
+	// testing for spaces in the phrase
+	// there is more spaces in the middle of the phrase
+	TEST(tappityTest, tappity_plus)
+	{
+		sentence R("i enjoy computer science");
+		string input = "i enjoy  computer science";
+	}
+	// testing for special characters
+	// dont being tested
+	TEST(tappityTest, tappity_plus)
+	{
+		sentence R("I don't know");
+		string input = "I dont know";
+	}
+	// testing for special characters
+	//  specifically capitalized 'i'
+	TEST(tappityTest, tappity_plus)
+	{
+		sentence R("I don't know");
+		string input = "i don't know";
+	}
+	// testing for old school of texting
+	TEST(tappityTest, tappity_plus)
+	{
+		practice obj;
+		sentence R("OMG did u c nw Utub video?");
+		string input = "OMG did u c nw Utub video ?";
+	}
+	// texting correctly
+	TEST(tappityTest, tappity_plus)
+	{
+		practice obj;
+		sentence R("OMG did u c nw Utub video?");
+		string input = "omg did you see new youtube video?";
+	}
+	TEST(tappityTest, tappity_plus)
+	{
+		sentence R("new pizza @ chico park");
+		string input = "new pizza @ chico ";
+	}
+	// testing for a very long paragraph
+	TEST(tappityTest, tappity_plus)
+	{
+		sentence R("while scrolling on instagram #lazydogs are much less cooler than #brownfoxes");
+		string input = "while scrolling on instagram #lazydogs are much less cooler than #brownfoxes ";
+	}
+	// testing for the period at the end
+	TEST(tappityTest, tappity_plus)
+	{
+		sentence R("A cats outfit is best accessorized when holding snacks with pink cosmopolitan and friend Chihuahua.");
+		string input = "A cats outfit is best accessorized when holding snacks with pink cosmopolitan and friend Chihuahua";
+	}
